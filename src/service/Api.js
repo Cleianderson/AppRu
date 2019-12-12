69ea3a5ed6a0998629b1bbf806b523032e1c0165
Cleianderson <cleianderson13@gmail.com>
2019-12-12 15:15:42 -0200

    Back to Heroku

--- src/service/Api.js
+++ src/service/Api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-//https://backendru.herokuapp.com
-const api = axios.create({ baseURL: 'http://192.168.1.103:9999' })
+
+const api = axios.create({ baseURL: 'https://backendru.herokuapp.com' })
 
 export default api
