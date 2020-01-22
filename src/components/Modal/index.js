17c0f6a10889e293d4e72204999c60fa871f9f30
Cleianderson <cleianderson13@gmail.com>
2020-01-22 09:55:23 -0200

    feat: out click for modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Modal, View } from 'react-native'
+import { Modal, View, TouchableWithoutFeedback } from 'react-native'
+import constants from '../../service/constants'
 
 export default function Modals(props) {
   return (
@@ -9,15 +10,22 @@ export default function Modals(props) {
       animationType='fade'
       onRequestClose={props.close}
     >
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignContent: 'center',
-          backgroundColor: 'rgba(27, 45, 79, .85)'
-        }} >
-        {props.component}
-      </View>
+      <TouchableWithoutFeedback onPress={props.close}>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignContent: 'center',
+            backgroundColor:
+              `rgba(${parseInt(constants.PRIMARY_COLOR.slice(1, 3), 16)}, 
+            ${parseInt(constants.PRIMARY_COLOR.slice(3, 5), 16)},
+            ${parseInt(constants.PRIMARY_COLOR.slice(5), 16)}, .85)`
+          }} >
+          <View onStartShouldSetResponder={() => true} >
+            {props.component}
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
     </Modal>
   )
 }
