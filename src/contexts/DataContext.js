c999fa5e7da62bdecc38b42dd628bfc807cc91a6
Cleianderson <cleianderson13@gmail.com>
2020-05-21 07:21:13 -0300

    refact: using more things in context

--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -1,10 +1,19 @@
-import {createContext} from 'react'
+import {createContext, Component} from 'react'
 
 const DataContext = createContext({
-    warns: [{title:'string', content:'string'}],
-    foods:[['string']],
-    favorites: ['string'],
-    day: 0,
+  warns: [{title: 'string', content: 'string'}],
+  foods: [['string']],
+  favorites: ['string'],
+  day: 0,
+  addFavorites: (str = '') => {},
+  removeFavorites: (str = '') => {},
+  setFavorites: (str = '') => {},
+  thereIsWarn: false,
+  updateThereIsWarn: (bool = false) => {},
+  setDay: (d = 0) => {},
+  homeViewPage: Component,
+  setHomeViewPage: (comp = Component) => {},
+  reload: () => {},
 })
 
 export default DataContext
