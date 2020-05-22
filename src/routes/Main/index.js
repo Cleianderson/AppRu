8e2cf714f7621805f80eaf2f5c74f13d068a6f74
Cleianderson <cleianderson13@gmail.com>
2020-05-22 08:05:44 -0300

    refact: more harmony in styles

--- src/routes/Main/index.js
+++ src/routes/Main/index.js
@@ -14,7 +14,7 @@ const RouteHome = () => {
     <Stack.Navigator>
       <Stack.Screen name="Home" component={Home} options={{title: ''}} />
       <Stack.Screen name="Info" component={Info} options={{title: 'Informações'}} />
-      <Stack.Screen name="Cardápio" component={Menu} />
+      <Stack.Screen name="Cardápio" component={Menu} options={{header: () => null}} />
       <Stack.Screen name="Config" component={Config} options={{title: 'Configurações'}} />
     </Stack.Navigator>
   )
