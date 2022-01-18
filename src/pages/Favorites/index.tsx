49ab2d37ba4f946d73f30ca452cb213c37a2e1d6
Cleianderson <cleianderson13@gmail.com>
2022-01-18 01:32:30 -0300

    feat: empty state icons

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from 'react'
+import Svg, { SvgProps, G, Path, Rect } from "react-native-svg"
 import { FlatList } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import { useDispatch, useSelector } from 'react-redux'
@@ -49,7 +50,49 @@ const Favorites = () => {
           )}
           ListEmptyComponent={() => (
             <EmptyContainer>
-              <EmptyText>Sua lista de favoritos está vazia</EmptyText>
+              <Svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={200}
+                height={200}
+                viewBox="0 0 135.467 135.467"
+              >
+                <G transform="translate(-62.148 -371.019) scale(1.92618)">
+                  <Path
+                    fill="#2f4e89"
+                    d="m68.946 205.398 24.29 8.097.475 27.151-24.291-8.097z"
+                  />
+                  <Path
+                    fill="#1f335a"
+                    d="m68.946 205.398.474 27.151-28.569 9.523-.474-27.15z"
+                  />
+                  <Path
+                    fill="#1b2d4f"
+                    d="m69.42 232.549 24.29 8.097-28.568 9.523-24.29-8.097z"
+                  />
+                  <Path
+                    fill="#294376"
+                    d="m93.235 213.494-28.567 9.523.473 27.15 28.568-9.522zM74.008 225.99l2.186 4.125 5.09-1.022-3.77 5.154.765 5.116-4.516-.939-4.617 4.184.98-5.735-3.619-2.53 5.12-2.606zm12.797 6.689.978 1.846 2.278-.458-1.687 2.307.342 2.29-2.021-.42-2.066 1.872.438-2.567-1.62-1.132 2.293-1.166z"
+                  />
+                  <Path
+                    fill="#325493"
+                    d="m40.377 214.92.474 27.15 24.29 8.097-.473-27.15zm11.88 9.698 1.932 5.12 5.163 2.053-4.098 2.562.447 5.2-4.466-3.535-4.886 1.16 1.338-4.747-3.467-4.483 5.293.602z"
+                  />
+                  <Rect
+                    width={30.114}
+                    height={11.159}
+                    x={-94.357}
+                    y={247.166}
+                    fill="#4c5a80"
+                    ry={0}
+                    transform="matrix(.94868 -.31623 .6238 .78158 0 0)"
+                  />
+                  <Path
+                    fill="#4c5a80"
+                    d="m40.377 214.922 24.29 8.097-5.715 8.308-24.29-8.097z"
+                  />
+                </G>
+              </Svg>
+              <EmptyText>sua lista de favoritos está vazia</EmptyText>
             </EmptyContainer>
           )}
           contentContainerStyle={{ flexGrow: 1 }}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import React, { useContext, useRef, useEffect } from 'react'
+import Svg, { SvgProps, G, Path, Rect, Circle } from "react-native-svg"
 import { View } from 'react-native'
 import ViewPage from '@react-native-community/viewpager'
 
@@ -51,8 +52,141 @@ const Home = () => {
             ))
           ) : (
             <EmptyContainer>
-              <EmptyText>;(</EmptyText>
-              <EmptyText style={{ fontSize: 16 }}>desculpa, não encontrei a semana</EmptyText>
+              <Svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={200}
+                height={200}
+                viewBox="0 0 135.467 135.467"
+              >
+                <G transform="translate(0 -161.533)">
+                  <Path
+                    fill="#b8c4f0"
+                    d="M43.97 191.624a3.208 3.208 0 0 0-3.872 2.385l-15.74 66.229a3.208 3.208 0 0 0 2.385 3.871l50.923 12.103a3.208 3.208 0 0 0 3.872-2.385l15.74-66.229a3.208 3.208 0 0 0-2.386-3.872z"
+                  />
+                  <Path
+                    fill="#4c5a80"
+                    d="M24.142 188.811a3.208 3.208 0 0 0-2.788 3.593l8.522 67.538a3.208 3.208 0 0 0 3.593 2.787l51.929-6.552a3.208 3.208 0 0 0 2.787-3.593l-8.521-67.537a3.208 3.208 0 0 0-3.593-2.788z"
+                  />
+                  <Rect
+                    width={21.015}
+                    height={3.299}
+                    x={1.863}
+                    y={235.329}
+                    fill="#9a9a9a"
+                    ry={1.65}
+                    transform="rotate(-7.191)"
+                  />
+                  <Rect
+                    width={21.015}
+                    height={3.299}
+                    x={1.863}
+                    y={210.827}
+                    fill="#9a9a9a"
+                    ry={1.65}
+                    transform="rotate(-7.191)"
+                  />
+                  <Rect
+                    width={21.015}
+                    height={3.299}
+                    x={1.863}
+                    y={218.994}
+                    fill="#9a9a9a"
+                    ry={1.65}
+                    transform="rotate(-7.191)"
+                  />
+                  <Rect
+                    width={21.015}
+                    height={3.299}
+                    x={1.863}
+                    y={243.497}
+                    fill="#9a9a9a"
+                    ry={1.65}
+                    transform="rotate(-7.191)"
+                  />
+                  <Rect
+                    width={21.015}
+                    height={3.299}
+                    x={1.863}
+                    y={251.664}
+                    fill="#9a9a9a"
+                    ry={1.65}
+                    transform="rotate(-7.191)"
+                  />
+                  <Rect
+                    width={21.015}
+                    height={3.299}
+                    x={1.863}
+                    y={227.162}
+                    fill="#9a9a9a"
+                    ry={1.65}
+                    transform="rotate(-7.191)"
+                  />
+                  <Rect
+                    width={21.015}
+                    height={3.299}
+                    x={30.094}
+                    y={235.329}
+                    fill="#9a9a9a"
+                    ry={1.65}
+                    transform="rotate(-7.191)"
+                  />
+                  <Rect
+                    width={21.015}
+                    height={3.299}
+                    x={30.094}
+                    y={210.827}
+                    fill="#9a9a9a"
+                    ry={1.65}
+                    transform="rotate(-7.191)"
+                  />
+                  <Rect
+                    width={21.015}
+                    height={3.299}
+                    x={30.094}
+                    y={218.994}
+                    fill="#9a9a9a"
+                    ry={1.65}
+                    transform="rotate(-7.191)"
+                  />
+                  <Rect
+                    width={21.015}
+                    height={3.299}
+                    x={30.094}
+                    y={243.497}
+                    fill="#9a9a9a"
+                    ry={1.65}
+                    transform="rotate(-7.191)"
+                  />
+                  <Rect
+                    width={21.015}
+                    height={3.299}
+                    x={30.094}
+                    y={251.664}
+                    fill="#9a9a9a"
+                    ry={1.65}
+                    transform="rotate(-7.191)"
+                  />
+                  <Rect
+                    width={21.015}
+                    height={3.299}
+                    x={30.094}
+                    y={227.162}
+                    fill="#9a9a9a"
+                    ry={1.65}
+                    transform="rotate(-7.191)"
+                  />
+                  <Circle cx={77.582} cy={226.562} r={23.974} fill="#b7d6dfc5" />
+                  <Path
+                    fill="#6387ca"
+                    d="M77.513 200.423a26.002 26.002 0 0 0-26.002 26.002 26.002 26.002 0 0 0 26.002 26.001 26.002 26.002 0 0 0 26.001-26.001 26.002 26.002 0 0 0-26.001-26.002zm0 5.62a20.381 20.381 0 0 1 20.38 20.382 20.381 20.381 0 0 1-20.38 20.38 20.381 20.381 0 0 1-20.382-20.38 20.381 20.381 0 0 1 20.382-20.381z"
+                  />
+                  <Path
+                    fill="#6387ca"
+                    d="M89.095 246.593s10.044 23.083 20.505 23.878c1.98.15 4.367-1.9 4.517-3.879.767-10.09-19.778-23.03-19.778-23.03z"
+                  />
+                </G>
+              </Svg>
+              <EmptyText style={{ fontSize: 16 }}>sem cardápio, ainda</EmptyText>
             </EmptyContainer>
           )}
         </ViewPage>
